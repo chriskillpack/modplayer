56ffe131fb0efbed3a982daeb75277e0e54064dd
Chris Killpack <chris@chriskillpack.com>
2023-01-22 21:48:14 -0800

    Restore modplay display output
    Removed when experimentation with Comb filters started.
    One-time allocate a scratch array between audio generation and filter input.

--- cmd/modplay/main.go
+++ cmd/modplay/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/chriskillpack/modplayer"
+	"github.com/fatih/color"
 	"github.com/gordonklaus/portaudio"
 )
 
@@ -149,12 +153,13 @@ func main() {
 	}()
 
 	c := newCombAdd(100*1024, 0.2, 150, *flagHz)
+	scratch := make([]int16, 10*1024)
 
 	var stream *portaudio.Stream
 	streamCB := func(out []int16) {
-		x := make([]int16, len(out))
-		player.GenerateAudio(x)
-		c.InputSamples(x)
+		sc := scratch[:len(out)]
+		player.GenerateAudio(sc)
+		c.InputSamples(sc)
 		n := c.GetAudio(out)
 		if n == 0 {
 			player.Stop()
@@ -170,6 +175,106 @@ func main() {
 	stream.Start()
 	defer stream.Stop()
 
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, syscall.SIGINT)
+	go func() {
+		<-sigch
+		player.Stop()
+		stream.Stop()
+		portaudio.Terminate()
+
+		fmt.Print(showCursor)
+		os.Exit(0)
+	}()
+
+	// Hide the cursor
+	fmt.Print(hideCursor)
+
+	white := color.New(color.FgWhite).SprintFunc()
+	cyan := color.New(color.FgCyan).SprintfFunc()
+	magenta := color.New(color.FgMagenta).SprintfFunc()
+	yellow := color.New(color.FgYellow).SprintfFunc()
+	blue := color.New(color.FgHiBlue).SprintFunc()
+
+	// Print out some player preceeding 4 rows, current row and upcoming 4 rows
+	// <title> row 1A/3F pat 0A/73 speed 6 bpm 125
+	//
+	//          0 000|     0 C00|     0 000|     0 000
+	//          0 000|     0 000|     0 000|     0 000
+	//     C#5  F 000|G-5 14 000|     0 000|     0 000
+	//          0 000|     0 000|     0 000|     0 000
+	// >>>      0 000|     0 000|     0 000|     0 000 <<<
+	//          0 000|     0 000|     0 000|     0 000
+	//          0 000|G-5 14 C0B|     0 000|     0 000
+	//          0 000|     0 000|     0 000|     0 000
+	//     C#5  F 000|     0 000|     0 000|     0 000
+
+	var lastState modplayer.PlayerState
 	for player.IsPlaying() {
+		state := player.State()
+
+		if lastState.Notes != nil && lastState.Order == state.Order && lastState.Row == state.Row {
+			continue
+		}
+
+		if len(song.Title) > 0 {
+			fmt.Print(song.Title + " ")
+		}
+		fmt.Printf("%s %02X/3F %s %02X/%02X %s %d %s %d\n", blue("row"), state.Row, blue("pat"), state.Order, len(song.Orders), blue("speed"), player.Speed, blue("bpm"), player.Tempo)
+
+		// Print out some channel info
+		ncl := len(state.Channels) / 2
+		for i, ch := range state.Channels {
+			outs := fmt.Sprintf("%2d: ", i+1)
+
+			si := ch.Instrument
+			if si != -1 {
+				outs += song.Samples[si].Name
+			}
+			if len(outs) < 26 {
+				outs = fmt.Sprintf("%-26s", outs)
+			}
+			fmt.Print(outs)
+			if i&1 == 1 {
+				fmt.Println()
+			}
+		}
+		fmt.Println()
+
+		for i := -4; i <= 4; i++ {
+			nd := player.NoteDataFor(state.Order, state.Row+i)
+			if nd == nil {
+				fmt.Println()
+				continue
+			}
+
+			// If this is the currently playing row then highlight it
+			if i == 0 {
+				fmt.Print(">>> ")
+			} else {
+				fmt.Print("    ")
+			}
+
+			// Print out the first 4 channels of note data
+			for ni, n := range nd {
+				if ni < 4 {
+					fmt.Print(white(n.Note), " ", cyan("%2X", n.Instrument), " ", magenta("%X", n.Effect), yellow("%02X", n.Param))
+					if ni < 3 {
+						fmt.Print("|")
+					}
+				} else if ni == 4 {
+					fmt.Print(" ...")
+					break
+				}
+			}
+			if i == 0 {
+				fmt.Print(" <<<")
+			}
+			fmt.Println()
+		}
+		fmt.Printf(escape+"%dF", 11+ncl) // move cursor to beginning of line 9 above
 	}
+
+	// Show the cursor
+	fmt.Print(showCursor)
 }

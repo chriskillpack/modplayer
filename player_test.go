2a5727d9f337973a980da7c7999412fc72be7510
Chris Killpack <chris@chriskillpack.com>
2023-03-15 16:29:35 -0700

    Add test for note decoding

--- player_test.go
+++ player_test.go
@@ -15,6 +15,7 @@ func TestLoadSong(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	if song.Title != "space_debris" {
 		t.Errorf("Incorrect song title %s", song.Title)
 	}
@@ -29,16 +30,58 @@ func TestLoadSong(t *testing.T) {
 	}
 }
 
+func TestNoteDataFor(t *testing.T) {
+	player, err := newTestPlayerFromMod("testdata/notes.mod")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if player.Song.Channels != 4 {
+		t.Errorf("expected 4 channel MOD, got %d", player.Song.Channels)
+	}
+
+	type testnote struct {
+		note       string
+		instrument int
+	}
+	expected := []struct {
+		row   int
+		notes []testnote
+	}{
+		{0, []testnote{
+			testnote{"C-4", 1},
+			testnote{"C#4", 2},
+			testnote{"D-4", 3},
+			testnote{"D#4", 4},
+		}},
+		{1, []testnote{
+			testnote{"D-5", 1},
+			testnote{"D#5", 2},
+			testnote{"G-5", 3},
+			testnote{"G#5", 4},
+		}},
+		{2, []testnote{
+			testnote{"C-6", 1},
+			testnote{"C#6", 2},
+			testnote{"D-6", 3},
+			testnote{"E-6", 4},
+		}},
+	}
+	for _, ex := range expected {
+		ndf := player.NoteDataFor(0, ex.row)
+		for i, nd := range ndf {
+			if ex.notes[i].instrument != nd.Instrument {
+				t.Errorf("Note %d of row %d, expected instrument %d actual %d", i, ex.row, ex.notes[i].instrument, nd.Instrument)
+			}
+			if ex.notes[i].note != nd.Note {
+				t.Errorf("Note %d of row %d, expected note %s actual %s", i, ex.row, ex.notes[i].note, nd.Note)
+			}
+		}
+	}
+}
+
 func BenchmarkMixChannels(b *testing.B) {
-	mod, err := os.ReadFile("testdata/mix.mod")
-	if err != nil {
-		b.Fatal(err)
-	}
-	song, err := NewSongFromBytes(mod)
-	if err != nil {
-		b.Fatal(err)
-	}
-	player, err := NewPlayer(song, 44100)
+	player, err := newTestPlayerFromMod("testdata/mix.mod")
 	if err != nil {
 		b.Fatal(err)
 	}
@@ -49,4 +92,20 @@ func BenchmarkMixChannels(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		player.GenerateAudio(out) // internally this calls MixChannels
 	}
+}
+
+func newTestPlayerFromMod(file string) (*Player, error) {
+	mod, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	song, err := NewSongFromBytes(mod)
+	if err != nil {
+		return nil, err
+	}
+	player, err := NewPlayer(song, 44100)
+	if err != nil {
+		return nil, err
+	}
+	return player, nil
 }

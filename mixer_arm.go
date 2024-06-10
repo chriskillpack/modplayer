1c43947f0ac1cca7de3faad8be7787878adc02e6
Chris Killpack <chris@chriskillpack.com>
2024-06-09 23:25:40 -0700

    Fix the cgo errors
    go build is now successful

--- mixer_arm.go
+++
@@ -1,8 +0,0 @@
-package modplayer
-
-// #include "mixer_neon.h"
-import "C"
-
-func mixChannelsNew(nSamples, offset int) {
-	// MixChannels_NEON()
-}
--- mixer_neon.h
+++ mixer_neon.h
@@ -1,6 +1,8 @@
 #ifndef MIXER_NEON_H
 #define MIXER_NEON_H
 
+#include <stdint.h>
+
 void MixChannels_NEON(int16_t* out, int8_t* data, uint8_t lvol, uint8_t rvol, int len);
 
 #endif
